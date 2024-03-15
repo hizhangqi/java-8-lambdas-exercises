509e5723cde7cbe6484f0baccf267bf9ec982ac7
hizhangqi <hizhangqi@yahoo.com>
2024-03-15 23:36:40 +0800

    添加 joinbrix 考题

--- src/main/java/com/systeminfos/job/joinbrix/ShowMeBugPassword.java
+++ src/main/java/com/systeminfos/job/joinbrix/ShowMeBugPassword.java
@@ -2,6 +2,7 @@ package com.systeminfos.job.joinbrix;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class ShowMeBugPassword {
@@ -37,16 +38,45 @@ public class ShowMeBugPassword {
         return result;
     }
 
+    public String solution2(String password) {
+        String result = "weak";
+        password = Optional.ofNullable(password).orElse("").trim();
+
+        Map<String, Long> keyCountMap = Arrays.stream(password.split("")).collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+        long errorKeyCount = keyCountMap.values().stream().filter(e -> e >= 3).count();
+        if (errorKeyCount > 0) {
+            return result;
+        }
+
+        String lowerCase = password.toLowerCase();
+        String upperCase = password.toUpperCase();
+        //数字+大小写字母
+        String regex = "[a-zA-Z0-9]{8,22}";
+        boolean matches = password.matches(regex);
+        if (matches && !password.equals(lowerCase) && !password.equals(upperCase)) {
+            return "strong";
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         ShowMeBugPassword password = new ShowMeBugPassword();
 //        System.out.println(password.solution1("abcdefgH1"));
-        System.out.println(password.solution1("!@#abcdefgH1"));
+//        System.out.println(password.solution1("!@#abcdefgH1"));
 //        System.out.println(password.solution1("abcdefgh123456"));
 //        System.out.println(password.solution1("abcdefgh123456"));
 //        System.out.println(password.solution1("Abcdefgh1234561234567890"));
-        System.out.println(password.solution1("Abcdefgh123456AbA"));
-        System.out.println(password.solution1("Abcdefgh123456cbA"));
-        System.out.println(password.solution1("!Abcdefgh123456cbA"));
+//        System.out.println(password.solution1("Abcdefgh123456AbA"));
+//        System.out.println(password.solution1("Abcdefgh123456cbA"));
+//        System.out.println(password.solution1("!Abcdefgh123456cbA"));
+
+
+        System.out.println(password.solution2(null));
+        System.out.println(password.solution2("Abc1234"));
+        System.out.println(password.solution2("Abcdefgh1234567890qwer"));
+        System.out.println(password.solution2("Abcdefgh1234567890qwert"));
+        System.out.println(password.solution2("Abcdefgh123456cbA"));
+        System.out.println(password.solution2("!Abcdefgh123456cbA"));
     }
 
 }
