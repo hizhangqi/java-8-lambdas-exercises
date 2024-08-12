40ed8025883175ed5a5dafdbc2736a981e6a6e26
hizhangqi <hizhangqi@yahoo.com>
2024-08-12 16:13:13 +0800

    数组列表添加

--- src/main/java/com/systeminfos/interview/copy/Cat2.java
+++ src/main/java/com/systeminfos/interview/copy/Cat2.java
@@ -4,11 +4,16 @@ package com.systeminfos.interview.copy;
  * 深拷贝：拷贝对象和原始对象的引用类型引用不同的对象。
  */
 public class Cat2 implements Cloneable {
+
+
     private String name;
     private Person owner;
 
     @Override
     protected Object clone() throws CloneNotSupportedException {
+        if (1 == 1 & 2 > 1) {
+
+        }
         Cat2 c = null;
         c = (Cat2) super.clone();
         c.owner = (Person) owner.clone();//拷贝Person对象
@@ -23,6 +28,19 @@ public class Cat2 implements Cloneable {
         Cat2 cloneCat = (Cat2) c.clone();
         p.setName("大彬");
         System.out.println(cloneCat.owner.getName());
+
+        int a = 5; // 0101 in binary
+        int b = 3; // 0011 in binary
+        int result = a & b; // 0001 in binary, which is 1 in decimal
+        System.out.println(result);
+
+        boolean a1 = true;
+        boolean b1 = false;
+        if (a1 & b1) {
+            // 这个块不会执行，因为 b 是 false
+            System.out.println(22);
+        }
+        System.out.println(a1 & b1);
     }
     //output
     //程序员大彬
